4ca4ca340f29831324b391fa7662631b80dbebb6
yellowbravemountain <joshuaweeyongsun@gmail.com>
2024-12-01 05:07:33 +0800

    stopped unhighlights when changing 3d model

--- hdb-ui/src/component/LandscapeModel.js
+++ hdb-ui/src/component/LandscapeModel.js
@@ -1,45 +1,42 @@
-import React, { useState, useEffect, useRef , useMemo } from 'react';
+import React, { useEffect, useRef, useMemo } from "react";
 import * as THREE from "three";
-import { Canvas, useThree, useFrame } from "@react-three/fiber";
-import { OrbitControls, Html } from "@react-three/drei";
-import GrassAndConcrete from './GrassAndConcrete';
+import { useThree } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import GrassAndConcrete from "./GrassAndConcrete";
 import ModelLoader from "./ModelLoader";
 
-
-
 const LandscapeModel = ({
-    index,
-    plantModels,
-    gridArray, 
-    coordinatesObject,
-    surroundingContext,
-    layersData,
-    updateLayersData,
-    allowInteraction, 
-    hoveredLayer,
-    updateHoveredLayer,
-    selectedLayer,
-    updateSelectedLayer,
-    downloadModel
+  index,
+  plantModels,
+  gridArray,
+  coordinatesObject,
+  surroundingContext,
+  layersData,
+  updateLayersData,
+  allowInteraction,
+  hoveredLayer,
+  updateHoveredLayer,
+  selectedLayer,
+  updateSelectedLayer,
+  downloadModel,
 }) => {
-    // TODO:
-    // 1. Render the 3D model using gridArray, coordinatesObject and plantModels
-    const  scene  = new THREE.Scene();
-    const { camera } = useThree();
-    scene.background = new THREE.Color(0xffffff); // Set background to white
+  // TODO:
+  // 1. Render the 3D model using gridArray, coordinatesObject and plantModels
+  const scene = new THREE.Scene();
+  const { camera } = useThree();
+  scene.background = new THREE.Color(0xffffff); // Set background to white
 
-
-    const raycaster = useRef(new THREE.Raycaster());
-    const mouse = useRef(new THREE.Vector2());
-    const hoveredObjectRef = useRef(null);
+  const raycaster = useRef(new THREE.Raycaster());
+  const mouse = useRef(new THREE.Vector2());
+  const hoveredObjectRef = useRef(null);
 
   // Extract grid and coordinates from JSON
 
   useEffect(() => {
     // Add ambient and directional lighting
-    const ambientLight = new THREE.AmbientLight(0x404040, 5 ); // Slightly dimmer
+    const ambientLight = new THREE.AmbientLight(0x404040, 5); // Slightly dimmer
     scene.add(ambientLight);
-    
+
     const light = new THREE.DirectionalLight(0xffffff, 2.5);
     light.position.set(50, 100, 50);
     light.castShadow = true;
@@ -58,217 +55,218 @@ const LandscapeModel = ({
     scene.add(hemisphereLight);
 
     return () => {
-        scene.remove(ambientLight, light, hemisphereLight);
+      scene.remove(ambientLight, light, hemisphereLight);
     };
-}, [scene]);
-    
+  }, [scene]);
 
-useEffect(() => {
+  useEffect(() => {
     if (layersData.length === 0) {
-        const newLayersData = Object.entries(coordinatesObject).map(([coord, speciesID], idx) => {
-            const [y, x] = coord.replace(/[()]/g, "").split(",").map(Number); // Parse coordinates
-            return { layerID: idx, speciesID, coordinate: [x, y] }; // Ensure coordinate matches [x, y]
-        });
-        updateLayersData(newLayersData);
+      const newLayersData = Object.entries(coordinatesObject).map(
+        ([coord, speciesID], idx) => {
+          const [y, x] = coord.replace(/[()]/g, "").split(",").map(Number); // Parse coordinates
+          return { layerID: idx, speciesID, coordinate: [x, y] }; // Ensure coordinate matches [x, y]
+        }
+      );
+      updateLayersData(newLayersData);
     }
-}, [coordinatesObject, layersData, updateLayersData]);
+  }, [coordinatesObject, layersData, updateLayersData]);
 
-
-    useEffect(() => {
+  useEffect(() => {
     if (allowInteraction) {
-        const onMouseMove = (event) => {
+      const onMouseMove = (event) => {
         const rect = event.target.getBoundingClientRect();
         mouse.current.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
         mouse.current.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
 
         const intersects = raycaster.current.intersectObjects(
-            scene.children,
-            true
+          scene.children,
+          true
         );
         if (intersects.length > 0) {
-            const object = intersects[0].object;
-            if (object !== hoveredObjectRef.current) {
+          const object = intersects[0].object;
+          if (object !== hoveredObjectRef.current) {
             hoveredObjectRef.current = object;
             const layer = layersData.find(
-                (layer) => layer.coordinate.join() === object.position.join()
+              (layer) => layer.coordinate.join() === object.position.join()
             );
             updateHoveredLayer(layer ? layer.layerID : null);
-            }
-        } else {
-            hoveredObjectRef.current = null;
-            updateHoveredLayer(null);
-        }
-        };
-
-        const onMouseDown = (event) => {
-          const rect = event.target.getBoundingClientRect();
-          mouse.current.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
-          mouse.current.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
-  
-          raycaster.current.setFromCamera(mouse.current, camera);
-  
-          const intersects = raycaster.current.intersectObjects(scene.children, true);
-          if (intersects.length > 0) {
-              const object = intersects[0].object;
-  
-              const layer = layersData.find((layer) =>
-                  layer.coordinate.join() === object.position.join()
-              );
-  
-              if (layer) {
-                  updateSelectedLayer(layer.layerID === selectedLayer ? null : layer.layerID);
-              }
-          } else {
-              // Deselect layer if clicking on a blank space
-              updateSelectedLayer(null);
           }
+        } else {
+          hoveredObjectRef.current = null;
+          updateHoveredLayer(null);
+        }
       };
 
+      const onMouseDown = (event) => {
+        const rect = event.target.getBoundingClientRect();
+        mouse.current.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+        mouse.current.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
+
+        raycaster.current.setFromCamera(mouse.current, camera);
+
+        const intersects = raycaster.current.intersectObjects(
+          scene.children,
+          true
+        );
+        if (intersects.length > 0) {
+          const object = intersects[0].object;
+
+          const layer = layersData.find(
+            (layer) => layer.coordinate.join() === object.position.join()
+          );
+
+          if (layer) {
+            updateSelectedLayer(
+              layer.layerID === selectedLayer ? null : layer.layerID
+            );
+          }
+        } else {
+          // Deselect layer if clicking on a blank space
+          updateSelectedLayer(null);
+        }
+      };
 
       window.addEventListener("mousedown", onMouseDown);
 
       return () => {
-          window.removeEventListener("mousedown", onMouseDown);
-        };
+        window.removeEventListener("mousedown", onMouseDown);
+      };
     }
   }, [layersData, selectedLayer, updateSelectedLayer, scene, camera]);
 
+  useEffect(() => {
+    const handleClick = (event) => {
+      const rect = event.target.getBoundingClientRect();
+      mouse.current.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+      mouse.current.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
 
-    useEffect(() => {
-        const handleClick = (event) => {
-            const rect = event.target.getBoundingClientRect();
-            mouse.current.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
-            mouse.current.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
-    
-            raycaster.current.setFromCamera(mouse.current, camera); // Use the extracted camera
-    
-            const intersects = raycaster.current.intersectObjects(scene.children, true);
-            if (intersects.length > 0) {
-                const object = intersects[0].object;
-    
-                // Find the corresponding layerID
-                const layer = layersData.find((layer) =>
-                    layer.coordinate.join() === object.position.join()
-                );
-    
-                if (layer) {
-                    updateSelectedLayer(layer.layerID);
-                }
-            }
-        };
-    
-        window.addEventListener('click', handleClick);
-        return () => window.removeEventListener('click', handleClick);
-    }, [layersData, updateSelectedLayer, scene, camera]);
-    
-    
-    
+      raycaster.current.setFromCamera(mouse.current, camera); // Use the extracted camera
 
+      const intersects = raycaster.current.intersectObjects(
+        scene.children,
+        true
+      );
+      if (intersects.length > 0) {
+        const object = intersects[0].object;
 
+        // Find the corresponding layerID
+        const layer = layersData.find(
+          (layer) => layer.coordinate.join() === object.position.join()
+        );
 
-    useEffect(() => {
-        if (downloadModel) {
-            // TODO: Function to download the 3D object model
+        if (layer) {
+          updateSelectedLayer(layer.layerID);
         }
-    }, [downloadModel])
+      }
+    };
 
+    window.addEventListener("click", handleClick);
+    return () => window.removeEventListener("click", handleClick);
+  }, [layersData, updateSelectedLayer, scene, camera]);
 
+  useEffect(() => {
+    if (downloadModel) {
+      // TODO: Function to download the 3D object model
+    }
+  }, [downloadModel]);
 
-useEffect(() => {
+  useEffect(() => {
     // Reset highlights for all models
     Object.entries(coordinatesObject).forEach(([coord, speciesID]) => {
-        const [y, x] = coord.replace(/[()]/g, "").split(",").map(Number);
-        const modelName = `${speciesID}.glb`;
-        const model = plantModels[speciesID]?.clone();
+      const [y, x] = coord.replace(/[()]/g, "").split(",").map(Number);
+      const modelName = `${speciesID}.glb`;
+      const model = plantModels[speciesID]?.clone();
 
-        if (model) {
-            model.traverse((node) => {
-                if (node.isMesh) {
-                    node.material.emissive = new THREE.Color(0x000000); // Reset highlight
-                }
-            });
-        }
+      if (model) {
+        model.traverse((node) => {
+          if (node.isMesh) {
+            node.material.emissive = new THREE.Color(0x000000); // Reset highlight
+          }
+        });
+      }
     });
 
     // Apply highlight to the selected model
     if (selectedLayer !== null && layersData[selectedLayer]) {
-        const { coordinate, speciesID } = layersData[selectedLayer];
-        const [x, y] = coordinate;
+      const { coordinate, speciesID } = layersData[selectedLayer];
+      const [x, y] = coordinate;
 
-        Object.entries(coordinatesObject).forEach(([coord, id]) => {
-            const [cy, cx] = coord.replace(/[()]/g, "").split(",").map(Number);
-            if (id === speciesID && x === cx && y === cy) {
-                const model = plantModels[speciesID]?.clone();
-                if (model) {
-                    model.traverse((node) => {
-                        if (node.isMesh) {
-                            node.material.emissive = new THREE.Color(0xff0000); // Red highlight
-                        }
-                    });
-                }
-            }
-        });
+      Object.entries(coordinatesObject).forEach(([coord, id]) => {
+        const [cy, cx] = coord.replace(/[()]/g, "").split(",").map(Number);
+        if (id === speciesID && x === cx && y === cy) {
+          const model = plantModels[speciesID]?.clone();
+          if (model) {
+            model.traverse((node) => {
+              if (node.isMesh) {
+                node.material.emissive = new THREE.Color(0xff0000); // Red highlight
+              }
+            });
+          }
+        }
+      });
     }
-}, [selectedLayer, layersData, coordinatesObject, plantModels]);
+  }, [selectedLayer, layersData, coordinatesObject, plantModels]);
 
-        // Determine the key of the highlighted model
-        const highlightedModelKey = useMemo(() => {
-            if (selectedLayer !== null && layersData[selectedLayer]) {
-                const { coordinate } = layersData[selectedLayer];
-                return `(${coordinate[1]}, ${coordinate[0]})`; // Match the coordinate format in the JSON
-            }
-            return null;
-        }, [selectedLayer, layersData]);
+  // Determine the key of the highlighted model
+  const highlightedModelKey = useMemo(() => {
+    if (selectedLayer !== null && layersData[selectedLayer]) {
+      const { coordinate } = layersData[selectedLayer];
+      return `(${coordinate[1]}, ${coordinate[0]})`; // Match the coordinate format in the JSON
+    }
+    return null;
+  }, [selectedLayer, layersData]);
 
+  return (
+    <>
+      <OrbitControls
+        enableDamping
+        dampingFactor={0.25}
+        screenSpacePanning={false}
+        maxPolarAngle={Math.PI / 2}
+      />
+      <ambientLight intensity={5} color={0x404040} />
+      <directionalLight
+        intensity={2.5}
+        position={[50, 100, 50]}
+        castShadow
+        shadow-mapSize-width={4096}
+        shadow-mapSize-height={4096}
+        shadow-camera-left={-200}
+        shadow-camera-right={200}
+        shadow-camera-top={200}
+        shadow-camera-bottom={-200}
+        shadow-camera-near={0.5}
+        shadow-camera-far={500}
+        shadow-bias={-0.0005}
+      />
+      <hemisphereLight
+        intensity={0.6}
+        skyColor={0x87ceeb}
+        groundColor={0x444444}
+      />
 
-    return (
-      <>
-        <OrbitControls
-          enableDamping
-          dampingFactor={0.25}
-          screenSpacePanning={false}
-          maxPolarAngle={Math.PI / 2}
-        />
-        <ambientLight intensity={5} color={0x404040} />
-        <directionalLight
-          intensity={2.5}
-          position={[50, 100, 50]}
-          castShadow
-          shadow-mapSize-width={4096}
-          shadow-mapSize-height={4096}
-          shadow-camera-left={-200}
-          shadow-camera-right={200}
-          shadow-camera-top={200}
-          shadow-camera-bottom={-200}
-          shadow-camera-near={0.5}
-          shadow-camera-far={500}
-          shadow-bias={-0.0005}
-        />
-        <hemisphereLight
-          intensity={0.6}
-          skyColor={0x87ceeb}
-          groundColor={0x444444}
-        />
+      {/* Grass and Concrete */}
+      <GrassAndConcrete
+        grid={gridArray}
+        surroundingContext={surroundingContext}
+      />
 
-        {/* Grass and Concrete */}
-        <GrassAndConcrete
-          grid={gridArray}
-          surroundingContext={surroundingContext}
-        />
-
-        {/* 3D Model Loader */}
-        <ModelLoader
-    coordinates={coordinatesObject}
-    preloadedModels={plantModels}
-    highlightedModelKey={highlightedModelKey}
-    hoveredLayer={layersData.find((layer) => layer.layerID === hoveredLayer)} // Match hovered layer
-    selectedLayer={layersData.find((layer) => layer.layerID === selectedLayer)} // Match selected layer
-    layersData={layersData}
-    updateSelectedLayer={updateSelectedLayer}
-/>
-
-      </>
-    );
-}
+      {/* 3D Model Loader */}
+      <ModelLoader
+        coordinates={coordinatesObject}
+        preloadedModels={plantModels}
+        highlightedModelKey={highlightedModelKey}
+        hoveredLayer={layersData.find(
+          (layer) => layer.layerID === hoveredLayer
+        )} // Match hovered layer
+        selectedLayer={layersData.find(
+          (layer) => layer.layerID === selectedLayer
+        )} // Match selected layer
+        layersData={layersData}
+        updateSelectedLayer={updateSelectedLayer}
+      />
+    </>
+  );
+};
 
 export default LandscapeModel;
--- hdb-ui/src/pages/EditConfiguration.js
+++ hdb-ui/src/pages/EditConfiguration.js
@@ -1,148 +1,160 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Canvas } from '@react-three/fiber';
-import { usePreload } from '../context/preloadContext';
-import LandscapeModel from '../component/LandscapeModel';
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Canvas } from "@react-three/fiber";
+import { usePreload } from "../context/preloadContext";
+import LandscapeModel from "../component/LandscapeModel";
 
 const EditConfiguration = () => {
-    const location = useLocation();
-    const { compositionData, compositionLayerData } = location.state || {};
-    const { plantModels } = usePreload();
+  const location = useLocation();
+  const { compositionData, compositionLayerData } = location.state || {};
+  const { plantModels } = usePreload();
 
-    const [hoveredLayerID, setHoveredLayerID] = useState(null);
-    const [selectedLayerID, setSelectedLayerID] = useState(null);
-    const [editedCompositionCoordinates, setEditedCompositionCoordinates] = useState(
-        JSON.parse(JSON.stringify(compositionData['coordinates']))
+  const [hoveredLayerID, setHoveredLayerID] = useState(null);
+  const [selectedLayerID, setSelectedLayerID] = useState(null);
+  const [editedCompositionCoordinates, setEditedCompositionCoordinates] =
+    useState(JSON.parse(JSON.stringify(compositionData["coordinates"])));
+  const [editedCompositionLayerData, setEditedCompositionLayerData] = useState(
+    JSON.parse(JSON.stringify(compositionLayerData))
+  );
+
+  const [newModelID, setNewModelID] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLayerHighlight = (layerID) => {
+    setSelectedLayerID((prevLayerID) =>
+      prevLayerID === layerID ? null : layerID
     );
-    const [editedCompositionLayerData, setEditedCompositionLayerData] = useState(
-        JSON.parse(JSON.stringify(compositionLayerData))
-    );
+  };
 
-    const [newModelID, setNewModelID] = useState('');
-    const [error, setError] = useState('');
+  const handleModelSwap = () => {
+    if (selectedLayerID !== null) {
+      const selectedLayerIndex = editedCompositionLayerData.findIndex(
+        (layer) => layer.layerID === selectedLayerID
+      );
 
-    const handleLayerHighlight = (layerID) => {
-        setSelectedLayerID((prevLayerID) => (prevLayerID === layerID ? null : layerID));
-    };
+      if (selectedLayerIndex !== -1) {
+        const selectedLayer = editedCompositionLayerData[selectedLayerIndex];
+        const selectedCoordinateKey = `(${selectedLayer.coordinate[1]}, ${selectedLayer.coordinate[0]})`;
 
-    const handleModelSwap = () => {
-        if (selectedLayerID !== null) {
-            const selectedLayerIndex = editedCompositionLayerData.findIndex(
-                (layer) => layer.layerID === selectedLayerID
-            );
-
-            if (selectedLayerIndex !== -1) {
-                const selectedLayer = editedCompositionLayerData[selectedLayerIndex];
-                const selectedCoordinateKey = `(${selectedLayer.coordinate[1]}, ${selectedLayer.coordinate[0]})`;
-
-                if (!plantModels[newModelID]) {
-                    setError('Invalid model ID. Please provide a valid one.');
-                    return;
-                }
-
-                const updatedCoordinates = { ...editedCompositionCoordinates };
-                updatedCoordinates[selectedCoordinateKey] = parseInt(newModelID);
-                setEditedCompositionCoordinates(updatedCoordinates);
-
-                const updatedLayers = [...editedCompositionLayerData];
-                updatedLayers[selectedLayerIndex] = {
-                    ...selectedLayer,
-                    speciesID: parseInt(newModelID),
-                };
-                setEditedCompositionLayerData(updatedLayers);
-
-                setError('');
-            }
-        } else {
-            setError('No model is currently selected. Select a layer first.');
+        if (!plantModels[newModelID]) {
+          setError("Invalid model ID. Please provide a valid one.");
+          return;
         }
-    };
 
-    return (
-        <div style={{ display: 'flex', height: '100vh' }}>
-            <div style={{ width: '20%', overflowY: 'scroll', backgroundColor: '#333', color: '#fff', padding: '10px' }}>
-                <div style={{ marginBottom: '20px' }}>
-                    <input
-                        type="text"
-                        value={newModelID}
-                        onChange={(e) => setNewModelID(e.target.value)}
-                        placeholder="Enter new model ID"
-                        style={{
-                            width: '80%',
-                            padding: '5px',
-                            marginBottom: '10px',
-                            borderRadius: '4px',
-                            border: '1px solid #ccc',
-                        }}
-                    />
-                    <button
-                        onClick={handleModelSwap}
-                        style={{
-                            width: '100%',
-                            padding: '10px',
-                            backgroundColor: '#4CAF50',
-                            color: '#fff',
-                            border: 'none',
-                            borderRadius: '4px',
-                            cursor: 'pointer',
-                        }}
-                    >
-                        Swap Model
-                    </button>
-                    {error && <p style={{ color: 'red', marginTop: '10px' }}>{error}</p>}
-                </div>
+        const updatedCoordinates = { ...editedCompositionCoordinates };
+        updatedCoordinates[selectedCoordinateKey] = parseInt(newModelID);
+        setEditedCompositionCoordinates(updatedCoordinates);
 
-                {editedCompositionLayerData.map((layer) => (
-                  <div
-    key={layer.layerID}
-    onMouseEnter={() => setHoveredLayerID(layer.layerID)}
-    onMouseLeave={() => setHoveredLayerID(null)}
-    onClick={() => handleLayerHighlight(layer.layerID)} // Call the highlight handler
-    style={{
-        padding: '10px',
-        cursor: 'pointer',
-        backgroundColor:
-            selectedLayerID === layer.layerID
-                ? '#8BBD8B' // Highlight color for selected layer
-                : hoveredLayerID === layer.layerID
-                ? '#BBBBBB' // Highlight color for hovered layer
-                : 'transparent',
-        borderRadius: '5px',
-        marginBottom: '5px',
-    }}
->
-    Layer {layer.layerID}: Species {layer.speciesID}
-</div>
+        const updatedLayers = [...editedCompositionLayerData];
+        updatedLayers[selectedLayerIndex] = {
+          ...selectedLayer,
+          speciesID: parseInt(newModelID),
+        };
+        setEditedCompositionLayerData(updatedLayers);
 
+        setError("");
+      }
+    } else {
+      setError("No model is currently selected. Select a layer first.");
+    }
+  };
 
-                ))}
-            </div>
-
-            <div style={{ width: '80%', position: 'relative' }}>
-                <Canvas
-                    shadows
-                    style={{ width: '100%', height: '100%' }}
-                    camera={{
-                        position: [100, 100, 100],
-                        fov: 50,
-                    }}
-                >
-                    <LandscapeModel
-                        plantModels={plantModels}
-                        gridArray={compositionData['grid']}
-                        coordinatesObject={editedCompositionCoordinates}
-                        surroundingContext={compositionData['surrounding_context']}
-                        layersData={editedCompositionLayerData}
-                        allowInteraction={true}
-                        hoveredLayer={hoveredLayerID}
-                        updateHoveredLayer={setHoveredLayerID}
-                        selectedLayer={selectedLayerID}
-                        updateSelectedLayer={handleLayerHighlight}
-                    />
-                </Canvas>
-            </div>
+  return (
+    <div style={{ display: "flex", height: "100vh" }}>
+      <div
+        style={{
+          width: "20%",
+          overflowY: "scroll",
+          backgroundColor: "#333",
+          color: "#fff",
+          padding: "10px",
+        }}
+      >
+        <div style={{ marginBottom: "20px" }}>
+          <input
+            type="text"
+            value={newModelID}
+            onChange={(e) => setNewModelID(e.target.value)}
+            placeholder="Enter new model ID"
+            onMouseDown={(e) => e.stopPropagation()} // Use onMouseDown for consistent behavior
+            style={{
+              width: "80%",
+              padding: "5px",
+              marginBottom: "10px",
+              borderRadius: "4px",
+              border: "1px solid #ccc",
+            }}
+          />
+          <button
+            onMouseDown={(e) => e.stopPropagation()} // Use onMouseDown to avoid triggering parent
+            onClick={(e) => {
+              e.stopPropagation();
+              handleModelSwap();
+            }}
+            style={{
+              width: "100%",
+              padding: "10px",
+              backgroundColor: "#4CAF50",
+              color: "#fff",
+              border: "none",
+              borderRadius: "4px",
+              cursor: "pointer",
+            }}
+          >
+            Swap Model
+          </button>
+          {error && <p style={{ color: "red", marginTop: "10px" }}>{error}</p>}
         </div>
-    );
+
+        {editedCompositionLayerData.map((layer) => (
+          <div
+            key={layer.layerID}
+            onMouseEnter={() => setHoveredLayerID(layer.layerID)}
+            onMouseLeave={() => setHoveredLayerID(null)}
+            onClick={() => handleLayerHighlight(layer.layerID)} // Call the highlight handler
+            style={{
+              padding: "10px",
+              cursor: "pointer",
+              backgroundColor:
+                selectedLayerID === layer.layerID
+                  ? "#8BBD8B" // Highlight color for selected layer
+                  : hoveredLayerID === layer.layerID
+                  ? "#BBBBBB" // Highlight color for hovered layer
+                  : "transparent",
+              borderRadius: "5px",
+              marginBottom: "5px",
+            }}
+          >
+            Layer {layer.layerID}: Species {layer.speciesID}
+          </div>
+        ))}
+      </div>
+
+      <div style={{ width: "80%", position: "relative" }}>
+        <Canvas
+          shadows
+          style={{ width: "100%", height: "100%" }}
+          camera={{
+            position: [100, 100, 100],
+            fov: 50,
+          }}
+        >
+          <LandscapeModel
+            plantModels={plantModels}
+            gridArray={compositionData["grid"]}
+            coordinatesObject={editedCompositionCoordinates}
+            surroundingContext={compositionData["surrounding_context"]}
+            layersData={editedCompositionLayerData}
+            allowInteraction={true}
+            hoveredLayer={hoveredLayerID}
+            updateHoveredLayer={setHoveredLayerID}
+            selectedLayer={selectedLayerID}
+            updateSelectedLayer={handleLayerHighlight}
+          />
+        </Canvas>
+      </div>
+    </div>
+  );
 };
 
 export default EditConfiguration;
